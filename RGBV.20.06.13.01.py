ac27670162c2723e21643b47eb23c2282fd3c0d1
Gamgoum <theone.sherif@gmail.com>
2020-06-18 16:03:39 +0200

    hi

--- RGBV.20.06.13.01.py
+++ RGBV.20.06.13.01.py
@@ -12,36 +12,54 @@ class optmizer():
 
     def __init__(self,):
         random.seed(64)
-        pass
 
-    def creation_function(self, Xn):
-        r = 4
-        Xn1 = r*Xn*(1-Xn)
-        return(Xn1)
+        self.Grid_size=(220,220)
+        self.max_steps = 20
+        
+        self.cells = []
+        for row in range(0,self.Grid_size[0]):
+            self.cells.append([])
+            for col in range(0, self.Grid_size[1]):
+                self.cells[row].append([])
+                self.cells[row][col]=[0,0,0]
+        self.cells[110][110]=[0,110,0]            
+        self.oldcells = copy.deepcopy(self.cells)
+        self.neighbours =[[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
         pass
 
         
     def creat(self,):
-        Grid_size=(220,220)
-        max_steps = 20
+
         save = True
         
         if save:
             Steps={}
-        Xn = 0.1
-        for step in range(0, max_steps):
-            image = []
-            for row in range(0,Grid_size[0]):
-                image.append([])
-                for col in range(0, Grid_size[1]):
-                    image[row].append([])
-                    for RGB in range(0, 3):
-                        Xn1 = self.creation_function(Xn)
-                        image[row][col].append(255*Xn1)
-                        Xn = Xn1
-                        
-            
-            Steps[step] = image
+
+        
+        for step in range(0, self.max_steps):
+            for row in range(0,self.Grid_size[0]):
+                for col in range(0, self.Grid_size[1]):
+                    if self.oldcells[row][col][0]!=0 or \
+                       self.oldcells[row][col][1]!=0 or \
+                       self.oldcells[row][col][2]!=0:
+                        neighbours = copy.deepcopy(self.neighbours)
+                        random.shuffle(neighbours)
+                        for neighbour in neighbours:
+                            if (row+neighbour[0])>=0 and (row+neighbour[0])<self.Grid_size[0] and \
+                               (col+neighbour[1])>=0 and (col+neighbour[1])<self.Grid_size[1]:
+                                if self.oldcells[row+neighbour[0]][col+neighbour[1]][0] == 0 and \
+                                   self.oldcells[row+neighbour[0]][col+neighbour[1]][1] == 0 and \
+                                   self.oldcells[row+neighbour[0]][col+neighbour[1]][2] == 0:
+                                    self.cells[row+neighbour[0]][col+neighbour[1]] = copy.deepcopy(self.oldcells[row][col])
+                                    for RGB_value in self.cells[row+neighbour[0]][col+neighbour[1]]:
+                                        if random.uniform(0,1) <0.05:
+                                            if random.uniform(0,1) <=0.5:
+                                                RGB_value -= (225*0.05)
+                                            else:
+                                                RGB_value += (225*0.05)
+                                    break
+            self.oldcells = copy.deepcopy(self.cells)
+            Steps[step] = copy.deepcopy(self.oldcells)
             
         if save:
             with open('rossSteps.js', 'w') as fp:
